23f0aa463a001a56d32353f36d8473778891ede3
wuding <netjoin@gmail.com>
2018-08-30 03:06:29 +0800

    新增一个版本库的批处理命令

---
+++ docs/wuding/astrology_pull.bat
@@ -0,0 +1,7 @@
+D:
+cd D:\www\work\astrology
+set filename=logs\%date:~0,4%%date:~5,2%%date:~8,2%.log
+echo #  >>%filename%
+echo %date%%time% >>%filename%
+"C:\Program Files\Git\cmd\git.exe" pull origin master >>%filename% 2>&1
+exit
