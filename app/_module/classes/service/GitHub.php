d1a93fd888c34c95ebdff7ce0d9f2337338c7d38
wuding <netjoin@gmail.com>
2018-09-06 19:31:02 +0800

    git 增加分支支持

--- app/_module/classes/service/GitHub.php
+++ app/_module/classes/service/GitHub.php
@@ -11,6 +11,8 @@ class GitHub extends WebHooks
 		$payload = isset($_POST['payload']) ? $_POST['payload'] : urldecode(preg_replace('/^payload=/', '', $this->php_input));
 		$json = json_decode($payload);
 		$full_name = $json->repository->full_name;
+		$ref = explode('/', $json->ref);
+		$branch = ('master' != $ref[2]) ? '_' . $ref[2] : '';
 		
 		// 比较签名、切换事件	
 		$hash = "sha1=" . hash_hmac('sha1', $this->php_input, $this->secret);		
@@ -18,7 +20,7 @@ class GitHub extends WebHooks
 		if (0 === $this->cmp) {		
 			switch ($git_event) {
 				case 'push':
-					$this->result = $this->git_pull($full_name);
+					$this->result = $this->git_pull($full_name . $branch);
 					break;
 				case 'ping':
 					break;
