85676cee3586392e88c597618e16ddcff1685701
netjoin <netjoin@gmail.com>
2019-01-27 00:27:42 +0800

    新增 private 双向同步

---
+++ docs/sync-repos/private_pull.bat
@@ -0,0 +1,10 @@
+D:
+cd D:\www\work\wuding\private
+set filename=logs\%date:~0,4%%date:~5,2%%date:~8,2%.log
+echo #  >>%filename%
+echo %date%%time% >>%filename%
+"C:\Program Files\Git\cmd\git.exe" pull gitee master >>%filename% 2>&1
+echo ##  >>%filename%
+git push origin master >>%filename% 2>&1
+echo ###  >>%filename%
+exit
