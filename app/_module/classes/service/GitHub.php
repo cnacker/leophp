05ac986e8176a89442e201a1a90ad699d9dee672
netjoin <netjoin@gmail.com>
2019-01-26 23:51:08 +0800

    修复 Webhooks API 使用 ping 无分支 ref 情况

--- app/_module/classes/service/GitHub.php
+++ app/_module/classes/service/GitHub.php
@@ -11,8 +11,14 @@ class GitHub extends WebHooks
 		$payload = isset($_POST['payload']) ? $_POST['payload'] : urldecode(preg_replace('/^payload=/', '', $this->php_input));
 		$json = json_decode($payload);
 		$full_name = $json->repository->full_name;
-		$ref = explode('/', $json->ref);
-		$branch = ('master' != $ref[2]) ? '_' . $ref[2] : '';
+
+		// 分支
+		$branch = null;
+		if (isset($json->ref)) {
+			$ref = explode('/', $json->ref);
+			$branch = ('master' != $ref[2]) ? '_' . $ref[2] : '';
+		}
+		
 		
 		// 比较签名、切换事件	
 		$hash = "sha1=" . hash_hmac('sha1', $this->php_input, $this->secret);		
