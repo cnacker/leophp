08225a85065c294a7f38ea6fc1c544cfda8db575
netjoin <netjoin@gmail.com>
2019-01-29 03:33:55 +0800

    修改 wubenli/zufang 批处理文件

--- docs/wubenli/zufang_pull.bat
+++ docs/wubenli/zufang_pull.bat
@@ -1,7 +1,8 @@
 D:
-cd D:\www\work\mr-fact
+cd D:\www\work\wubenli\zufang
 set filename=logs\%date:~0,4%%date:~5,2%%date:~8,2%.log
 echo #  >>%filename%
 echo %date%%time% >>%filename%
+"C:\Program Files\Git\cmd\git.exe" pull origin master >>%filename% 2>&1
 echo ###  >>%filename%
 exit
